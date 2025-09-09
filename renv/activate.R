589906d23b44175ef283e28d7f28327d71f873b9
BenMerSci <benmercier20@icloud.com>
2025-09-08 22:53:56 -0400

    Updated renv

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- ..version..
-  attr(version, "sha") <- ..sha..
+  version <- "1.1.5"
+  attr(version, "sha") <- NULL
 
   # the project directory
   project <- Sys.getenv("RENV_PROJECT")
